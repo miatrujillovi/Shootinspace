f395e0aff5f59ab5c1b1fe37473f99225f2f2bb6
Mia Trujillo <151059107+miatrujillovi@users.noreply.github.com>
2025-07-10 09:45:11 -0600

    Early Implementation for Multiplier Boosts
    - Added functions to the PlayerMovement to work according to the multiplier script and give the boosts of sprinting and double jump.

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -2,9 +2,13 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [Header("Variables for Movement")]
+    [Header("Variables for Normal Movement")]
     [SerializeField] private float playerSpeed;
     [SerializeField] private float jumpForce;
+    [Space]
+    [Header("Variables for Boost Movement")]
+    [SerializeField] private bool sprint;
+    [SerializeField] private bool doubleJump;
 
     private Rigidbody rb;
     private Vector3 movement;
@@ -13,35 +17,25 @@ public class PlayerMovement : MonoBehaviour
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
+        sprint = false; doubleJump = false;
     }
 
+    //Player Movement Functions
     [System.Obsolete]
     private void FixedUpdate()
     {
+        if (sprint)
+        {
+            if (Input.GetKeyDown(KeyCode.LeftShift))
+            {
+                playerSpeed += 5;
+            }
+            else
+            {
+                playerSpeed -= 5;
+            }
+        }
         MovePlayer();
-    }
-
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
-        {
-            Jump();
-        }
-    }
-
-    private void Jump()
-    {
-        rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-    }
-
-    private bool IsGrounded()
-    {
-        RaycastHit hit;
-        if (Physics.Raycast(transform.position, Vector3.down, out hit, 1.1f))
-        {
-            return hit.collider.CompareTag("Ground");
-        }
-        return false;
     }
 
     [System.Obsolete]
@@ -55,5 +49,54 @@ public class PlayerMovement : MonoBehaviour
 
         velocity = new Vector3(movement.x * playerSpeed, rb.velocity.y, movement.z * playerSpeed);
         rb.velocity = velocity;
+    }
+
+    //Player Jumping Functions
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
+        {
+            Jump();
+        }
+    }
+
+    private void Jump()
+    {
+        if (doubleJump)
+        {
+            //Double Jump Here
+        }
+        rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+    }
+
+    private bool IsGrounded()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, Vector3.down, out hit, 1.1f))
+        {
+            return hit.collider.CompareTag("Ground");
+        }
+        return false;
+    }
+
+    //Boost Activation Functions
+    public void ActivateSprint()
+    {
+        sprint = true;
+    }
+
+    public void DeactivateSprint()
+    {
+        sprint = false;
+    }
+
+    public void ActivateDoubleJump()
+    {
+        doubleJump = true;
+    }
+
+    public void DeactivateDoubleJump()
+    {
+        doubleJump = false;
     }
 }
