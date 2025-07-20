968d049f73f3ffba0d06adf120052fcd14d00e5d
Mia Trujillo <151059107+miatrujillovi@users.noreply.github.com>
2025-07-20 10:41:55 -0600

    Main Menu Changes
    - Added Main Menu
    - Added Animations for Starting Game
    - Added VFX for cinematic

--- Assets/Scripts/Audio/AudioManager.cs
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -7,8 +7,7 @@ public class AudioManager : MonoBehaviour
     [Space]
     [Header("Music Audio References")]
     [SerializeField] private AudioClip fightMusic;
-    [SerializeField] private AudioClip introMusic;
-    [SerializeField] private AudioClip outOfNightmareMusic;
+    [SerializeField] private AudioClip interludeMusic;
     [Space]
     [Header("SFX Audio References")]
     [SerializeField] private AudioClip gunShot;
@@ -20,26 +19,10 @@ public class AudioManager : MonoBehaviour
     [SerializeField] private AudioClip fuelObtained;
     [SerializeField] private AudioClip buttonClick;
 
-    private bool onMainMenu;
-    private bool onGameplayFight;
-    private bool onGameplayEndOfEnemies;
-    private bool onPause;
-
-    private void Awake()
-    {
-        onMainMenu = true;
-        onGameplayFight = false;
-        onGameplayEndOfEnemies = false;
-        onPause = false;
-    }
-
     private void Start()
     {
-        if (onMainMenu)
-        {
-            musicSource.clip = introMusic;
-            musicSource.Play();
-        }
+        musicSource.clip = interludeMusic;
+        musicSource.Play();
     }
 
     public void PlaySFX(AudioClip clip)
--- Assets/Scripts/UI/ScenesManager.cs
+++ Assets/Scripts/UI/ScenesManager.cs
@@ -1,11 +1,37 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Unity.Cinemachine;
+using DG.Tweening;
+using System.Collections;
 
 public class ScenesManager : MonoBehaviour
 {
+    [SerializeField] private GameObject spaceShip;
+    [SerializeField] private CinemachineCamera cam;
+    [SerializeField] private Transform camTransform;
+    [SerializeField] private GameObject mainMenuScreen;
+    [SerializeField] private GameObject estela;
+ 
     public void StartGame()
     {
+        mainMenuScreen.SetActive(false);
+        estela.SetActive(true);
+        StartCoroutine("StartCinematic");
+    }
+
+    private IEnumerator StartCinematic()
+    {
+        camTransform.DOMoveZ(-230.9f, 3f).SetEase(Ease.InOutSine);
+
+        camTransform.DOMove(new Vector3(-140f, 11.1f, -48.8f), 3f).SetEase(Ease.InSine);
+        camTransform.DORotate(new Vector3(0f, 66.265f, 0f), 3f).SetEase(Ease.InSine);
+
+        spaceShip.transform.DOMove(spaceShip.transform.position + new Vector3(450, 0, 0), 3f).SetEase(Ease.InQuad);
+        estela.transform.DOMove(estela.transform.position + new Vector3(400, 0, 0), 3f).SetEase(Ease.InQuad);
+        yield return camTransform.DOMove(new Vector3(105.3f, 11.1f, -110.3f), 3f).SetEase(Ease.InQuint).WaitForCompletion();
+
         SceneManager.LoadScene("Level");
+        yield return new WaitForSeconds(0.1f);
     }
 
     public void ExitGame()
